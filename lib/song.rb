7611e0d314d8b04d1541d117f6b30a7ac8581d91
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-02-22 20:13:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,4 @@
 class Song 
-  attr_accessor :name, :artist 
-
-  @@all = []
-
-  def initialize(name, artist=nil)
-    @name = name
-    @artist = artist
-    @@all << self
-  end
-
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-
-  def self.all
-    @@all
-  end
-end
+  attr_accessor :name
+end 
+name = Song.new
